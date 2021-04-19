222f1cb63fc54c65912f0a1a19c8d9b23a5b0f78
heerock <heerock93@gmail.com>
2021-04-19 15:58:37 +0900

    add socket

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ require('dotenv').config()
 const run = () => {
 
     const url = process.env.SERVER_URL
+    const url_alerts = process.env.SERVER_ALERTS_URL
     setInterval(() => {
         curl.get(url, {}, (err, res, body) => {
-            console.log('Curl Connect')
+            console.log('Curl Status Connect')
+        })
+
+    }, 1000)
+
+    setInterval(() => {
+        curl.get(url_alerts, {}, (err, res, body) => {
+            console.log('Curl Alerts Connect')
         })
     }, 1000)
 }
