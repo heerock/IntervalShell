be49047485510e93ffbc0e7a6819c7ba5f194996
heerock <heerock93@gmail.com>
2021-05-18 17:24:56 +0900

    layoutStatus Add

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const run = () => {
 
     const url = process.env.SERVER_URL
     const url_alerts = process.env.SERVER_ALERTS_URL
+    const url_layout = process.env.SERVER_LAYOUT_URL
     setInterval(() => {
         curl.get(url, {}, (err, res, body) => {
             console.log('Curl Status Connect')
@@ -14,6 +15,12 @@ const run = () => {
     setInterval(() => {
         curl.get(url_alerts, {}, (err, res, body) => {
             console.log('Curl Alerts Connect')
+        })
+    }, 1000)
+
+    setInterval(() => {
+        curl.get(url_layout, {}, (err, res, body) => {
+            console.log('Curl Layout Connect')
         })
     }, 1000)
 }
